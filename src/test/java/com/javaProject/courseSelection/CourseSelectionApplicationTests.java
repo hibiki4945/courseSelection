9d675a57c429fe67d2f996a419898a04cf900ebe
hibiki4945 <wc4910720@gmail.com>
2023-10-11 14:23:32 +0800

    original

---
+++ src/test/java/com/javaProject/courseSelection/CourseSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProject.courseSelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
